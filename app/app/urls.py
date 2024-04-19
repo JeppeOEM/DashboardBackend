6d7742f512073a16eb3a9df93d0cde6120da8c91
Jeppe <jepp026b@edu.zealand.dk>
2024-04-19 19:20:53 +0200

    auth

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,21 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+#is imported in the docker container
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 
 from django.contrib import admin
-from django.urls import path
+#include is used when getting endpoints from diffrent app
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #download the schema file
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path(
-        'api/docs/',
+    #swagger documentation url
+    path('api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
 ]
